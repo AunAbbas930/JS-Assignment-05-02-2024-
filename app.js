28ae369bb5207d2d1589f7a97564ec6780e945be
Aun Abbas <153765137+AunAbbas930@users.noreply.github.com>
2024-02-05 02:26:41 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -7,12 +7,16 @@ alert('Welcome to my website!');
 function wrongPassword() 
 {
 var pass = prompt('Enter the Password:');
-document.write("Error! Please Enter a valid password.<br><br>(Refresh page to go back)");
+document.write("<html><head><link rel='stylesheet' href='style.css'></head><body><button class='btn' onClick='location.reload()'>");
+document.write("Error! Please Enter a valid password.");
+document.write("<button></body>");
 }
 
 function message1() 
 {
+document.write("<html><head><link rel='stylesheet' href='style.css'></head><body><button class='btn' onClick='location.reload()'>");
 document.write("Hello Dear!<br><br>Hope you in fine condition.<br>Please refresh this page to go back.<br><br>Regards,<br>Aun Abbas");
+document.write("<button></body>");
 }
 
 function threeAlerts() 
@@ -30,6 +34,31 @@ alert("This alert is being displayed as part of this website.");
 function addElements()
 {
 alert("After closing this dialogue, see how the title (head) changes.");
+var newElement = document.createElement('title'); 
+newElement.textContent = 'See! This Changed.'; 
+var oldElement = document.getElementById('title'); 
+oldElement.replaceWith(newElement);
+
+alert("See how an element is added before html in body. (See Navbar)");
+var tag = document.createElement("a"); 
+var text = document.createTextNode("[Addition before HTML]"); 
+tag.appendChild(text); 
+var element = document.getElementById("navbar"); 
+element.appendChild(tag);
+
+alert("See how an element is added in html body. (See button of Q6)");
+var tag = document.createElement("p"); 
+var text = document.createTextNode("[Addition in body]"); 
+tag.appendChild(text); 
+var element = document.getElementById("addBody"); 
+element.appendChild(tag);
+
+alert("See how an element is added at end of body. (A button is added after Q25)");
+var tag = document.createElement("button"); 
+var text = document.createTextNode("[Addition after HTML]"); 
+tag.appendChild(text); 
+var element = document.getElementById("questions"); 
+element.appendChild(tag);
 }
 
 function declareUsername()
@@ -76,16 +105,20 @@ alert(message);
 
 function messageBrowser()
 {
+document.write("<html><head><link rel='stylesheet' href='style.css'></head><body><button class='btn' onClick='location.reload()'>");
 var message = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
 document.write(message);
+document.write("<button></body>");
 }
 
 
 function messageAlertBrowser()
 {
+document.write("<html><head><link rel='stylesheet' href='style.css'></head><body><button class='btn' onClick='location.reload()'>");
 var message = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
 alert(message);
 document.write(message);
+document.write("<button></body>");
 }
 
 function age()
@@ -93,6 +126,19 @@ function age()
 var age = prompt("Enter your age (years): ");
 age = "Your age is " + age + " years";
 alert(age);
+}
+
+function visits()
+{
+document.write("<html><head><link rel='stylesheet' href='style.css'></head><body><button class='btn' onClick='location.reload()'>");
+var visitCount = 1;
+if (localStorage.getItem("visitCount")) 
+  {
+  visitCount = parseInt(localStorage.getItem("visitCount")) + 1;
+  }
+localStorage.setItem("visitCount", visitCount);
+document.write("You have visited this site " + visitCount + " times.");
+document.write("<button></body>");
 }
 
 function birthYear()
@@ -104,11 +150,13 @@ alert(age);
 
 function clothingStore()
 {
+document.write("<html><head><link rel='stylesheet' href='style.css'></head><body><button class='btn' onClick='location.reload()'>");
 var name = prompt("Enter Name of Customer:");
 var product = prompt("Enter name of ordered product:");
 var qty = prompt("Enter quantity of ordered product:");
 var message = name + " ordered " + qty + " " + product + " on XYZ Clothing Store.";
 document.write(message);
+document.write("<button></body>");
 }
 
 function legalVariables()
@@ -119,9 +167,11 @@ variable1 = "Aun Abbas";
 variable2 = 1601;
 variable3 = true;
 
+document.write("<html><head><link rel='stylesheet' href='style.css'></head><body><button class='btn' onClick='location.reload()'>");
 document.writeln("Variable 1: " + variable1 + "<br>");
 document.writeln("Variable 2: " + variable2 + "<br>");
 document.writeln("Variable 3: " + variable3);
+document.write("<button></body>");
 }
 
 function illegalVariables()
@@ -138,6 +188,7 @@ var illegal_$price = "variable@symbol";
 var space = "variable with space";
 var keyword = "break"; 
 
+document.write("<html><head><link rel='stylesheet' href='style.css'></head><body><button class='btn' onClick='location.reload()'>");
 document.write("<b>Legal Variable Names:</b><br><br>");
 document.write("1. " + legalVariable1 + "<br>");
 document.write("2. " + legal_variable_2 + "<br>");
@@ -151,14 +202,17 @@ document.write("2. " + illegal_variable_2 + "<br>");
 document.write("3. " + illegal_$price + "<br>");
 document.write("4. " + space + "<br>");
 document.write("5. " + keyword + "<br>");
+document.write("<button></body>");
 }
 
 function rulesNaming()
 {
+document.write("<html><head><link rel='stylesheet' href='style.css'></head><body><button class='btn' onClick='location.reload()'>");
 document.write("<p> Variable names can only contain letters, numbers, underscores, and dollar signs. For example $my_1stVariable</p>");
 document.write("<p> Variables must begin with a letter, underscore, or dollar sign. For example $name, _name, or name</p>");
 document.write("<p> Variable names are case-sensitive</p>");
 document.write("<p> Variable names should not be JS keywords</p>");
+document.write("<button></body>");
 }
 
 function add()
@@ -168,7 +222,9 @@ var number2 = +prompt("Enter the second number:");
 
 var add = number1 + number2;
 
+document.write("<html><head><link rel='stylesheet' href='style.css'></head><body><button class='btn' onClick='location.reload()'>");
 document.write("Addition result: " + add);
+document.write("<button></body>");
 }
 
 function multiOperations()
@@ -181,14 +237,17 @@ var multiply = number1 * number2;
 var divide = number1 / number2;
 var mod = number1 % number2;
 
+document.write("<html><head><link rel='stylesheet' href='style.css'></head><body><button class='btn' onClick='location.reload()'>");
 document.write("Subtraction result: " + subtract + "<br>");
 document.write("Multiplication result: " + multiply + "<br>");
 document.write("Division result: " + divide + "<br>");
 document.write("Modulus result: " + mod + "<br>");
+document.write("<button></body>");
 }
 
 function q23()
 {
+document.write("<html><head><link rel='stylesheet' href='style.css'></head><body><button class='btn' onClick='location.reload()'>");
 var myVariable;
 document.write("Value after variable declaration is: " + myVariable + "<br>");
 myVariable = 5;
@@ -201,28 +260,34 @@ myVariable--;
 document.write("Value after decrement is: " + myVariable + "<br>");
 var remainder = myVariable % 3;
 document.write("The remainder is: " + remainder + "<br>");
+document.write("<button></body>");
 }
 
 function movieTicket()
 {
+document.write("<html><head><link rel='stylesheet' href='style.css'></head><body><button class='btn' onClick='location.reload()'>");
 var ticketPrice = 600;
 var totalCost = ticketPrice * 5;
 document.write("<p>The cost of buying 5 movie tickets is: PKR " + totalCost);
+document.write("<button></body>");
 }
 
 function table()
 {
+document.write("<html><head><link rel='stylesheet' href='style.css'></head><body><button class='btn' onClick='location.reload()'>");
 var num = +prompt("Enter a number:");
 document.write("<b>Multiplication Table for " + num + ":</b><br><br>");
 for (var i = 1; i <= 10; i++) {
     var result = num * i;
     document.write(num + " x " + i + " = " + result + "<br>");
     }
+document.write("<button></body>");
 }
 
 function a201()
 {
 var a = +prompt("Assign a value (number) to a:");
+document.write("<html><head><link rel='stylesheet' href='style.css'></head><body><button class='btn' onClick='location.reload()'>");
 document.write("Result:<br>The value of a is: " + a + "<br>.....................................<br><br>");
 document.write("<br>The value of ++a is: " + ++a);
 document.write("<br>Now the value of a is: " + a);
@@ -236,10 +301,12 @@ document.write("<br>");
 document.write("<br>The value of a-- is: " + a--);
 document.write("<br>Now the value of a is: " + a);
 document.write("<br>");
+document.write("<button></body>");
 }
 
 function a202()
 {
+document.write("<html><head><link rel='stylesheet' href='style.css'></head><body><button class='btn' onClick='location.reload()'>");
 document.write("<b>Output of:</b>--a<br>............................<br>");
 var a = 2, b = 1; 
 document.write("a = " + a + "<br>b = " + b + "<br><br>");
@@ -275,6 +342,7 @@ document.write("<br>It decreased the value by 1, after using variable in code. R
 document.write("<br>Value of 'a' is reduced by 1, which becomes 1. Value of 'b' is also reduced by 1, and becomes 0.");
 document.write("<br>Again a prefix increment '++b' adds 1 to 'b' and 'b' becomes 1. At last, appears 'b--' which is a postfix decrement.");
 document.write("As postfix acts after usage of variable, value of 'b' doesn't change and the results appears as 1 - 0 + 1 + 1, which is 3.<br><br><br>");
+document.write("<button></body>");
 }
 
 function a203()
@@ -291,11 +359,13 @@ if(num===0)
   {
   num = 5;
   }
+document.write("<html><head><link rel='stylesheet' href='style.css'></head><body><button class='btn' onClick='location.reload()'>");
 document.write("<b>Multiplication Table for " + num + ":</b><br><br>");
 for (var i = 1; i <= 10; i++) {
     var result = num * i;
     document.write(num + " x " + i + " = " + result + "<br>");
     }
+document.write("<button></body>");
 }
 
 function a205()
@@ -307,11 +377,13 @@ var marks1 = +prompt("Enter marks of 1st Subject:");
 var marks2 = +prompt("Enter marks of 2nd Subject:");
 var marks3 = +prompt("Enter marks of 3rd Subject:");
 var total = marks1 + marks2 + marks3;
+document.write("<html><head><link rel='stylesheet' href='style.css'></head><body><button class='btn' onClick='location.reload()'>");
 document.write("<table><tr><td><b>Subject</b></td><td><b>Total Marks</b></td><td><b>Obtained Marks</b></td><td><b>Percentage</b></td></tr>");
 document.write("<tr><td>" + sub1 + "</td><td>100</td><td>" + marks1 + "</td><td>" + marks1 + "%</td></tr>");
 document.write("<tr><td>" + sub2 + "</td><td>100</td><td>" + marks2 + "</td><td>" + marks2 + "%</td></tr>");
 document.write("<tr><td>" + sub3 + "</td><td>100</td><td>" + marks3 + "</td><td>" + marks3 + "%</td></tr>");
 document.write("<tr><td><b>Total</b></td><td><b>300</b></td><td><b>" + total + "</td><td><b>" + total/3 + "%</b></td></tr></table>");
+document.write("<button></body>");
 }
 
 function karachi()
@@ -343,4 +415,159 @@ if(fuel < 0.25)
   {
   alert("Please refill the fuel in your car!");
   }
+}
+
+function a304()
+{
+var a = 4;
+if (++a === 5)
+  {
+  alert("given condition for variable a is true");
+  }
+  
+var b = 82;
+if(b++ === 83)
+  {
+  alert("given condition for variable b is true");
+  }
+  
+var c = 12;
+if (c++ === 13)
+  {
+  alert("condition 1 is true");
+  }
+if (c === 13)
+  {
+  alert("condition 2 is true");
+  }
+if (++c < 14)
+  {
+  alert("condition 3 is true");
+  }
+if(c === 14)
+  {
+  alert("condition 4 is true");
+  }
+  
+var materialCost = 20000;
+var laborCost = 2000;
+var totalCost = materialCost + laborCost;
+if (totalCost === laborCost + materialCost)
+  {
+  alert("The cost equals");
+  }
+if (true)
+  {
+  alert("True");
+  } 
+if (false)
+  {
+  alert("False");
+  }
+if("car" < "cat")
+  {
+  alert("car is smaller than cat");
+  }
+}
+
+function randomNum()
+{
+var randomNum = Math.floor(Math.random() * 10);
+var guess = +prompt("Enter your guess (1 - 10):");
+if(guess === randomNum)
+  {
+  alert("Bingo! Correct Answer.");
+  }
+if(guess+1 === randomNum || guess-1 === randomNum)
+  {
+  alert("Close Enough to the Correct Answer.");
+  }
+}
+
+function div3()
+{
+var num = +prompt("Enter a number:");
+var rem = num%3;
+if(rem===0)
+  {
+  alert(num + " is divisible by 3.");
+  }
+else
+  {
+  alert(num + " is not divisible by 3.");
+  }
+}
+
+
+function evenOdd()
+{
+var num = +prompt("Enter a number:");
+var rem = num%2;
+if(rem===0)
+  {
+  alert(num + " is an even number.");
+  }
+else
+  {
+  alert(num + " is an odd number.");
+  }
+}
+
+
+function temp()
+{
+var temp = +prompt("Enter the temperature:");
+if(temp > 40)
+  {
+  alert("It is too hot outside.");
+  }
+if(temp > 30 && temp < 40) 
+  {
+  alert("The Weather today is Normal.");
+  }
+if(temp > 20 && temp < 30)
+  {
+  alert("Today’s Weather is cool.");
+  }
+if(temp > 10 && temp < 20)
+  {
+  alert("OMG! Today’s weather is so Cool.");
+  }
+}
+
+function calculate() 
+{
+var num1 = +prompt("Enter 1st number:");
+var num2 = +prompt("Enter 2nd number:");
+var op = prompt("Enter operator (+ - * / %):");
+if(op === "+")
+  {
+  var answer = num1 + num2;
+  message = num1 + " + " + num2 + " = " + answer;
+  }
+else if(op === "-")
+  {
+  var answer = num1 - num2;
+  message = num1 + " - " + num2 + " = " + answer;
+  }
+else if(op === "*")
+  {
+  var answer = num1 * num2;
+  message = num1 + " x " + num2 + " = " + answer;
+  }
+else if(op === "/")
+  {
+  var answer = num1 / num2;
+  message = num1 + " / " + num2 + " = " + answer;
+  }
+else if(op === "%")
+  {
+  var answer = num1 % num2;
+  message = num1 + " % " + num2 + " = " + answer;
+  }
+else
+  {
+  message = "Incorrect operator entered! please try again.";
+  }
+alert(message);
 }
